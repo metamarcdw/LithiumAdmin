4680324d95eca12c114626480a3718538fd8580c
Marc Wood <marcdw87@gmail.com>
2019-12-06 00:01:49 -0500

    Initial commit

---
+++ lithiumadmin/admin.py
@@ -0,0 +1,62 @@
+from django.contrib import admin
+from django.db.models.fields.mixins import NOT_PROVIDED
+from django.utils.encoding import force_text
+
+from .models import Customer, Case
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("name", "prod_version", "prod_hosting")
+    list_filter = ("prod_hosting",)
+
+
+class OpenCasesFilter(admin.SimpleListFilter):
+    not_closed = "QUE,INP,NOT"
+    title = "status"
+    parameter_name = "status"
+
+    def lookups(self, request, model_admin):
+        return (
+            (OpenCasesFilter.not_closed, "Not closed"),
+            ("QUE", "Queued"),
+            ("INP", "In Progress"),
+            ("NOT", "Waiting on another Team"),
+            ("CLO", "Closed / Resolved")
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value in ("QUE", "INP", "NOT", "CLO"):
+            return queryset.filter(status__exact=value)
+        return queryset.filter(status__in=OpenCasesFilter.not_closed.split(","))
+
+    def choices(self, changelist):
+        for lookup, title in self.lookup_choices:
+            yield {
+                "selected": self.value() == force_text(lookup),
+                "query_string": changelist.get_query_string(
+                    {self.parameter_name: lookup}, []
+                ),
+                "display": title,
+            }
+
+
+class CaseAdmin(admin.ModelAdmin):
+    list_display = (
+        "salesforce_case",
+        "customer",
+        "title",
+        "priority",
+        "severity",
+        "date_requested",
+        "status",
+        "report_me",
+    )
+    list_filter = (OpenCasesFilter, "priority", "severity", "report_me")
+    list_editable = ("report_me",)
+    search_fields = ("salesforce_case", "title")
+    ordering = ("severity", "priority", "-date_requested")
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Case, CaseAdmin)
---
+++ lithiumadmin/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    ON_PREMISE = "ONP"
+    HOSTED = "HOS"
+    SAAS = "SAA"
+    HOSTING_CHOICES = [(ON_PREMISE, "On-Premise"), (HOSTED, "Hosted"), (SAAS, "SaaS")]
+
+    name = models.CharField(max_length=50, unique=True)
+    prod_hosting = models.CharField(max_length=3, choices=HOSTING_CHOICES, default=SAAS)
+    prod_version = models.CharField(max_length=10)
+
+    def __str__(self):
+        on_prem = "# " if self.prod_hosting == Customer.ON_PREMISE else ""
+        return f"{on_prem}{self.name}"
+
+
+class Case(models.Model):
+    QUEUED = "QUE"
+    IN_PROGRESS = "INP"
+    NOT_LITHIUM = "NOT"
+    CLOSED = "CLO"
+    STATUS_CHOICES = [
+        (QUEUED, "Queued with Lithium"),
+        (IN_PROGRESS, "In Progress"),
+        (NOT_LITHIUM, "Waiting on another Team"),
+        (CLOSED, "Closed / Resolved"),
+    ]
+
+    CRITICAL = "CRI"
+    HIGH = "HI"
+    MEDIUM = "MED"
+    LOW = "LOW"
+    PRIORITY_CHOICES = [
+        (CRITICAL, "Critical"),
+        (HIGH, "High"),
+        (MEDIUM, "Medium"),
+        (LOW, "Low"),
+    ]
+
+    SEV1 = "SEV1"
+    SEV2 = "SEV2"
+    SEV3 = "SEV3"
+    SEVERITY_CHOICES = [
+        (SEV1, "Severity 1"),
+        (SEV2, "Severity 2"),
+        (SEV3, "Severity 3"),
+    ]
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=100)
+    salesforce_case = models.CharField(max_length=10, unique=True)
+
+    date_entered = models.DateField(auto_now_add=True)
+    date_requested = models.DateField()
+
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default=QUEUED)
+    priority = models.CharField(max_length=3, choices=PRIORITY_CHOICES, default=MEDIUM)
+    severity = models.CharField(max_length=4, choices=SEVERITY_CHOICES, default=SEV3)
+
+    is_billable = models.BooleanField(default=False)
+    project_code = models.CharField(max_length=20, blank=True)
+
+    files_location = models.CharField(max_length=300, blank=True)
+
+    customer_notes = models.TextField()
+    analyst_notes = models.TextField(blank=True)
+    developer_notes = models.TextField(blank=True)
+    my_notes = models.TextField(blank=True)
+
+    report_me = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.salesforce_case}: ({self.customer.name}) - {self.title}"
